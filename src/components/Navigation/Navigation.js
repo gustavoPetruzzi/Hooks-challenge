34fe669c51c9a9bf84bf836057ea3e60822400d9
unknown <Gustavo Petruzzi@SVARLP2018159.ar.softvision.global>
2020-01-31 14:05:13 -0300

    responsive navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,25 +2,24 @@ import React from 'react';
 import classes from './Navigation.module.css';
 import logo from '../../assets/logo.png';
 import cart from '../../assets/shopping-cart.png';
-const Navigation = (props) =>{
-    let price;
-    let algo = [10,20,30,40];
-    
-    
+const Navigation = (props) =>{    
     return(
         <header className={classes.Header}>
             <div>
                 <img src={logo} alt="Ezshop" />
             </div>
             <div style={{marginLeft:'10px'}}>
-                <p> EzReal</p>
+                <p className={classes.Item}> Ezshop</p>
             </div>
             <nav className={classes.Navigation}>
                 <ul className={classes.Items}>
                     <li className={classes.Item}>
                        <p> $ {(props.cart.reduce((acc,cur) => acc + cur.price, 0).toFixed(2))} </p>
                     </li>
-                    <li>
+                    <li className={classes.Cart}>
+                        <div className={classes.CartObject}>
+                            {props.cart.length} 
+                        </div>
                         <img src={cart} alt="cart" />
                     </li>
                 </ul>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './Product.module.css';
 const Product = (props) =>{
-
+     
     return(
         <div className={classes.Product}>
             <img alt={props.name} src={props.image} />
-            <p className={classes.Name} >{props.name}</p>
-            <p> $ {props.price}</p>
+            <h3  className={classes.Name} >{props.name}</h3>
+            <h4> $ {props.price.toFixed(2)}</h4>
 
             <button className={classes.ButtonItem} onClick={props.clicked}>Agregar Producto</button>    
              
